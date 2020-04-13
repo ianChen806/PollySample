ff28e362af359084a22ed9333418fd25247a9f6a
Ian <ian.chen@taiwantaxi.com.tw>
2020-04-13 15:12:48 +0800

    add AddTransientHttpErrorPolicy.

--- PollySample/Controllers/WeatherForecastController.cs
+++ PollySample/Controllers/WeatherForecastController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -17,23 +15,19 @@ namespace PollySample.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IHttpClientFactory _clientFactory;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger,IHttpClientFactory clientFactory)
         {
             _logger = logger;
+            _clientFactory = clientFactory;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<HttpResponseMessage> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-                             {
-                                 Date = DateTime.Now.AddDays(index),
-                                 TemperatureC = rng.Next(-20, 55),
-                                 Summary = Summaries[rng.Next(Summaries.Length)]
-                             })
-                             .ToArray();
+            var httpClient = _clientFactory.CreateClient("Test");
+            return await httpClient.GetAsync("http://tasdasdxcsswweest.com");
         }
     }
 }
--- PollySample/Startup.cs
+++ PollySample/Startup.cs
@@ -1,15 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Polly;
 
 namespace PollySample
 {
@@ -26,6 +21,19 @@ namespace PollySample
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddHttpClient("Test", client =>
+                    {
+                    })
+                    .AddTransientHttpErrorPolicy(builder =>
+                    {
+                        return builder.WaitAndRetryAsync(new[]
+                        {
+                            TimeSpan.FromSeconds(1),
+                            TimeSpan.FromSeconds(5),
+                            TimeSpan.FromSeconds(10),
+                        });
+                    });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
